3a9d506fc4ed12e3f5a755f9503a9b27bce6b2c3
Seum <mostofaseum8@gmail.com>
2026-02-18 23:43:30 +0600

    lab task 2 done

--- src/reviewer/reviewer.dto.ts
+++ src/reviewer/reviewer.dto.ts
@@ -1,11 +1,25 @@
-import { isEmail, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {  IsEmail , Matches, MinLength, IsIn, IsPhoneNumber } from 'class-validator';
 export class CreateReviewerDto {
     name: string;
 
     @IsEmail()
+    @Matches(/^[a-zA-Z0-9._%+-]+@aiub\.edu$/, {
+    message: 'Email must be a valid @aiub.edu email'
+    })
     email: string;
 
+    @MinLength(6, {message: 'Password must be at least 6 characters long'})
+    @Matches(/[A-Z]/, {message: 'Password must contain at least one uppercase letter',})
     password: string;
+
+  @IsIn(['Male', 'Female'], {
+    message: 'Gender must be either Male or Female',})
+    gender: string;
+
+    @IsPhoneNumber('BD', {message: 'Phone number must be a valid phone number'})
+    phone_number :number;
+
+    
 } 
 export class LoginDto {
   email: string;
