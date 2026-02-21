e52bcbaafdd59b848f80bfc2108e3d4f671e70b6
Seum <mostofaseum8@gmail.com>
2026-02-22 00:31:43 +0600

    added controller

---
+++ src/reviewer/reviewer.controller.ts
@@ -0,0 +1,55 @@
+import { Controller, Get, Post, Put, Patch, Delete, Body, Param, ParseIntPipe, Query, ValidationPipe, UsePipes} from '@nestjs/common';
+import { ReviewerService } from './reviewer.service';
+import { CreateReviewerDto, LoginDto, UpdateProfileDto, VerifyWorkDto } from './reviewer.dto';
+
+@Controller('reviewer')
+export class ReviewerController {
+  constructor(private readonly reviewerService: ReviewerService) {}
+  @Get()
+  getHello(): string {
+    return this.reviewerService.getHello();
+  }
+  @Post('signup') //localhost:3000/reviewer/signup
+  @UsePipes(new ValidationPipe())
+  signup(@Body() reviewerDto:CreateReviewerDto):object {
+    return this.reviewerService.signup(reviewerDto);
+  }
+@Post('login')  //localhost:3000/reviewer/login
+login(@Body() loginDto:LoginDto):object {
+    return this.reviewerService.login(loginDto);
+  }
+
+  @Get('profile/:id')  //localhost:3000/reviewer/profile/1
+  getProfile(@Param('id') id:number):object {
+    return this.reviewerService.getProfile(id);
+  }
+
+  @Put('profile/:id')  //localhost:3000/reviewer/profile/1
+  updateProfile(
+    @Param('id', ParseIntPipe) id: number, 
+    @Body() updateProfileDto: UpdateProfileDto
+  ) {
+    return this.reviewerService.updateProfile(id, updateProfileDto);
+  }
+  @Get('verify-users')    //localhost:3000/reviewer/verify-users?type=student
+  getUsersToVerify(@Query('type') type: string) {
+    return this.reviewerService.getUsersToVerify(type);
+  }
+  @Patch('verify-users/:id')  //localhost:3000/reviewer/verify-users/1
+  verifyUser(@Param('id', ParseIntPipe) id: number) {
+    return this.reviewerService.verifyUser(id);
+  }
+
+  @Post('/work/:workId/review')  //localhost:3000/reviewer/work/1/review
+  reviewWork(
+    @Param('workId', ParseIntPipe) workId: number,
+    @Body() verifyWorkDto: VerifyWorkDto
+  ) {
+    return this.reviewerService.reviewWork(workId, verifyWorkDto);
+  }
+
+@Delete('/reports/:reportId')   //localhost:3000/reviewer/reports/1
+  resolveReport(@Param('reportId', ParseIntPipe) reportId: number) {
+    return this.reviewerService.resolveReport(reportId);
+  }
+}
