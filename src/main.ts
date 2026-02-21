b8e6bfd9c10d453e5747c65fccc8645302f19aac
Helal-AIUB <helalaiub8009@gmail.com>
2026-02-21 22:45:13 +0600

    Implemented Data Validation using NestJs Pipes (User Category 4) LabTask-02

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
+
+
+//Postman Testing - No Error(201)
+// {
+//   "name": "Helal Uddin",
+//   "password": "pass@word",
+//   "dob": "2000-01-01",
+//   "socialMediaLink": "https://linkedin.com/in/helal"
+// }
+
+
+//Postman Testing -Error (400)
+// {
+//   "name": "Helal123", 
+//   "password": "mypassword",
+//   "dob": "not-a-date",
+//   "socialMediaLink": "facebook"
+// }
--- src/worker/dto/create-worker.dto.ts
+++ src/worker/dto/create-worker.dto.ts
@@ -1,5 +1,25 @@
+// export class CreateWorkerDto {
+//   name: string;
+//   email: string;
+//   skills: string[];
+// }
+
+import { IsString, IsNotEmpty, Matches, IsDateString, IsUrl, NotContains, IsAlpha } from 'class-validator';
+
 export class CreateWorkerDto {
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-zA-Z\s]*$/, { message: 'Name should not contain any numbers' }) // Rule: No numbers [cite: 29]
   name: string;
-  email: string;
-  skills: string[];
+
+  @IsNotEmpty()
+  @Matches(/[@#\$&]/, { message: 'Password must contain @ or # or $ or &' }) // Rule: Special char [cite: 30]
+  @IsString()
+  password: string;
+
+  @IsDateString({}, { message: 'Please provide a valid date' }) // Rule: Valid date type [cite: 31]
+  dob: string;
+
+  @IsUrl({}, { message: 'Please provide a valid social media URL' }) // Rule: URL format [cite: 32]
+  socialMediaLink: string;
 }
