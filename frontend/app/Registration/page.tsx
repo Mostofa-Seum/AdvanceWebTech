9a0427cf35ce786b51d843f80359ab2fe6f0cced
Seum <mostofaseum8@gmail.com>
2026-04-29 21:27:53 +0600

    feat: implement registration page with Zod validation and base layout setup. I mean lab task done

--- frontend/app/Registration/page.tsx
+++ frontend/app/Registration/page.tsx
@@ -2,8 +2,7 @@
 
 import { useState, FormEvent, JSX } from "react";
 import { z } from "zod";
-import Layout from "@/components/Layout/layout";
-import Title from "@/components/Layout/title";
+import Layout from "../layout";
 
 // Zod schema
 const loginSchema = z.object({
@@ -15,44 +14,86 @@ const loginSchema = z.object({
     .string()
     .min(1, "Password is required")
     .min(6, "Password must be at least 6 characters"),
+  
+
+  fullName: z.string()
+  .min(3, { message: "Name must be at least 3 characters long" })
+  .max(50, { message: "Name cannot exceed 50 characters" }),
+
+  age: z.coerce.number()
+  .int({ message: "Age must be a whole number" })
+  .min(18, { message: "You must be at least 18 years old to register" })
+  .max(120, { message: "Please enter a valid age" }),
+
+  confirmpassword: z.string(),
+})
+.refine((data) => data.password === data.confirmpassword, {
+  message: "Passwords do not match",
+  path: ["confirmpassword"],
 });
 
-type LoginData = z.infer<typeof loginSchema>;
+type RegistrationData = z.infer<typeof loginSchema>;
 
-export default function LoginPage(): JSX.Element {
+
+export default function RegistrationPage(): JSX.Element {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [confirmpassword, setConfirmPassword] = useState<string>("");
+  const [fullName, setFullName] = useState<string>("");
+  const [age, setAge] = useState<number | "">("");
   const [error, setError] = useState<string>("");
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
 
-    const result = loginSchema.safeParse({ email, password });
+    const result = loginSchema.safeParse({ fullName, age, email, password, confirmpassword });
 
     if (!result.success) {
-      setError(result.error.errors[0].message);
+      setError(result.error.issues[0].message);
       return;
     }
 
     console.log(result.data);
+    setFullName("");
+    setAge("");
     setEmail("");
     setPassword("");
+    setConfirmPassword("");
     setError("");
   };
 
   return (
     <>
-      <Title page="Login" />
       <Layout>
-        <h1>Login</h1>
-
+        <center><h5 className="text-2xl font-bold mb-4">Registration</h5></center>
         <form onSubmit={handleSubmit}>
+            <div>
+            <label>Name</label>
+            <input
+              type="text"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              className="border border-gray-400 rounded px-3 py-2 w-full text-black mb-4 mt-1"/>
+             </div>
+
+
+            <div>
+            <label>Age</label>
+            <input
+              type="number"
+              value={age}
+              onChange={(e) => setAge(e.target.value === "" ? "" : Number(e.target.value))}
+              className="border border-gray-400 rounded px-3 py-2 w-full text-black mb-4 mt-1"/>
+          </div>
+
+
           <div>
             <label>Email</label>
             <input
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              className="border border-gray-400 rounded px-3 py-2 w-full text-black mb-4 mt-1"
             />
           </div>
 
@@ -62,12 +103,23 @@ export default function LoginPage(): JSX.Element {
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              className="border border-gray-400 rounded px-3 py-2 w-full text-black mb-4 mt-1"
+            />
+          </div>
+
+            <div>
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              value={confirmpassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="border border-gray-400 rounded px-3 py-2 w-full text-black mb-4 mt-1"
             />
           </div>
 
           {error && <p>{error}</p>}
 
-          <button type="submit">Login</button>
+          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Register</button>
         </form>
       </Layout>
     </>
